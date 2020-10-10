699ecdfdbda34f1f8a2df93aa86b9f0c630ffe2c
Tites Moore <tmoore@edgewebware.com>
2020-10-10 19:31:52 -0400

    Fixed files not adding to git, added .gitignore

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float horizontalForce = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    // FixedUpdate is best practice for physics
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("d")) {
+            horizontalForce = 500f;
+        } else if (Input.GetKey("a")) {
+            horizontalForce = -500f;
+        } else {
+            horizontalForce = 0f;
+        }
+
+        rb.AddForce(horizontalForce * Time.deltaTime, 0, forwardForce * Time.deltaTime);
+    }
+}
