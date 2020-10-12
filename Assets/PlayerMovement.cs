ec895dd0e9d467d443c20e4d79ff540350c2ae12
Tites Moore <tmoore@edgewebware.com>
2020-10-12 19:59:43 -0400

    Added player collision script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,7 +9,6 @@ public class PlayerMovement : MonoBehaviour
 
     public float forwardForce = 500f;
     public float horizontalForce = 0f;
-    public string userInput;
 
     // Start is called before the first frame update
     void Start()
