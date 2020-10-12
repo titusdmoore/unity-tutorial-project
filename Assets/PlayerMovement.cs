7a4527741121320f6cfea8316578c497195b41d5
Tites Moore <tmoore@edgewebware.com>
2020-10-12 18:25:49 -0400

    Added camera following functionality

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-    public float forwardForce = 0f;
+    public float forwardForce = 500f;
     public float horizontalForce = 0f;
     public string userInput;
 
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update() {
-        userInput = Input.inputString;
+        //userInput = Input.inputString;
     }
 
     // Update is called once per frame
@@ -25,47 +25,15 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
 
-        /*if (Input.GetKey("d")) {
+        if (Input.GetKey("d")) {
             horizontalForce = 500f;
         } else if (Input.GetKey("a")) {
             horizontalForce = -500f;
         } else {
             horizontalForce = 0f;
         }
+        
+        rb.AddForce(horizontalForce * Time.deltaTime, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("w")) {
-            forwardForce = 500f;
-        } else if (Input.GetKey("s")) {
-            forwardForce = -500f;
-        } else {
-            horizontalForce = 0f;
-        }*/
-
-        string[] inputArray = userInput.Split();
-
-        if (inputArray.Length > 0) {
-            foreach (var key in inputArray) {
-                switch (key) {
-                    case "w":
-                        forwardForce = 500f;
-                        break;
-                    case "a":
-                        horizontalForce = -500f;
-                        break;
-                    case "s":
-                        forwardForce = -500f;
-                        break;
-                    case "d":
-                        horizontalForce = -500f;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            rb.AddForce(horizontalForce * Time.deltaTime, 0, forwardForce * Time.deltaTime);
-        }
-
-        rb.AddForce(0, 0, 0);
     }
 }
