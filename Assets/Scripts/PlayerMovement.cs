6c46fd3fe8c3d1fe68d7f022515485b2f7cb6c47
Titus Moore <tmoore@edgewebware.com>
2020-10-12 21:48:53 -0400

    Added more obstacles, reworked controls again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,36 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     public Rigidbody rb;
 
-    public float forwardForce = 500f;
-    public float horizontalForce = 0f;
+    public float forwardForce;
+    public float horizontalForce;
+
+    private bool isMoving = false;
+    private bool isRight;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
     }
 
-    void Update() {
-        //userInput = Input.inputString;
+    void OnGUI() {
+
+        Event e = Event.current;
+
+        //Check the type of the current event, making sure to take in only the KeyDown of the keystroke.
+        //char.IsLetter to filter out all other KeyCodes besides alphabetical.
+        if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1 && char.IsLetter(e.keyCode.ToString()[0]) && (e.keyCode == KeyCode.A || e.keyCode == KeyCode.D)) {
+            isMoving = true;
+
+            if (e.keyCode == KeyCode.D) {
+                isRight = true;
+            } else {
+                isRight = false;
+            }
+        }
+
     }
+
 
     // Update is called once per frame
     // FixedUpdate is best practice for physics
-    void FixedUpdate()
-    {
-
-        if (Input.GetKey("d")) {
-            horizontalForce = 500f;
-        } else if (Input.GetKey("a")) {
-            horizontalForce = -500f;
-        } else {
-            horizontalForce = 0f;
-        }
-        
-        rb.AddForce(horizontalForce * Time.deltaTime, 0, forwardForce * Time.deltaTime);
-
+    void FixedUpdate() {
+        rb.AddForce(isMoving 
+            ? isRight 
+                ? horizontalForce * Time.deltaTime
+                : -horizontalForce * Time.deltaTime
+            : 0, 0, forwardForce * Time.deltaTime);
     }
 }
